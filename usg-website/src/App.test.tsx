9a28d06064c37fd126003ba88c05519349e98814
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-08 21:00:57 +0200

    add coming soon text for deployment

--- usg-website/src/App.test.tsx
+++ usg-website/src/App.test.tsx
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/We are USG! 🎮/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Our Website will be up soon!/i);
+  expect(linkElement).toBeInTheDocument();
+});
