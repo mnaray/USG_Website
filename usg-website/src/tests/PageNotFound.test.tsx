97fffaf62a7c40d276481abdac1e0f10db40fc74
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-11 16:53:14 +0200

    implement router and reorganize repo

---
+++ usg-website/src/tests/PageNotFound.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../pages/PageNotFound';
+import PageNotFound from '../pages/PageNotFound';
+
+test('renders 404 page', () => {
+    render(<PageNotFound />);
+    const errorText = screen.getByText(/Error 404: Page not found!/i);
+    expect(errorText).toBeInTheDocument();
+});
+
+// test('checks text color', () => {
+//     render(<PageNotFound />);
+//     const errorText: HTMLElement = screen.getByText(/Error 404: Page not found!/i);
+//     const style = getComputedStyle(errorText);
+//     expect(style).toBe('color: red');
+// });
