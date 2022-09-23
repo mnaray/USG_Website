23f9a7cca0e69b7d2f60d9b0874a154c925ec6ed
TheSolian <89124854+TheSolian@users.noreply.github.com>
2022-09-23 14:54:14 +0200

    Reaplce CSS with Tailwind

--- usg-website/src/pages/PageNotFound.tsx
+++ usg-website/src/pages/PageNotFound.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import '../css/App.css';
 import Logo from './components/Logo';
 
 function PageNotFound() {
     return (
         <div className='App'>
-            <header className="App-header">
-                <h1 className='Error'>Error 404: Page not found!</h1>
+            <header className="app-header">
+                <h1 className='text-red-600'>Error 404: Page not found!</h1>
                 <p>Bitte eine valide URL eingeben.</p>
                 <Logo />
             </header>
