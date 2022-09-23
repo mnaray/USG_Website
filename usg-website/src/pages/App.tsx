72357e70720c800194dfa7f5be11f94c7f474fde
n¿co <nico.delvecchio@gmx.ch>
2022-09-23 10:10:03 +0200

    add tailwind packages and first css edit

--- usg-website/src/pages/App.tsx
+++ usg-website/src/pages/App.tsx
@@ -1,18 +1,15 @@
-import React from 'react';
-import '../css/App.css';
-import Logo from './components/Logo';
+import React from "react";
+import "../css/App.css";
+import "../css/tailwind.css";
+import Logo from "./components/Logo";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
-        <p>
-          We are USG! 🎮
-        </p>
-        <p>
-          Our Website will be up soon!
-        </p>
+        <p>We are USG! 🎮</p>
+        <p className="text-red-400">Our Website will be up soon!</p>
       </header>
     </div>
   );
