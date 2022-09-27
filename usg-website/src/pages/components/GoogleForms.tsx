b542f1851a47a46e20f9856d93260f55ccdce958
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-27 21:12:56 +0200

    add title to iframe embed

--- usg-website/src/pages/components/GoogleForms.tsx
+++ usg-website/src/pages/components/GoogleForms.tsx
@@ -6,7 +6,7 @@ interface Source {
 
 function GoogleForms(source: Source) {
     return (
-        <iframe src={source.url} width="640" height="595" frameBorder="0" marginHeight={0} marginWidth={0} className="my-12 mx-6">
+        <iframe title="Google Forms embed" src={source.url} width="640" height="595" frameBorder="0" marginHeight={0} marginWidth={0} className="my-12 mx-6">
             Wird geladen…
         </iframe>
     )
