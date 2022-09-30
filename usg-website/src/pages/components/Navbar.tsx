002a4ccb658c0a3688141e3306d5fd05800fe662
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-30 10:49:24 +0200

    implement navbar

--- usg-website/src/pages/components/Navbar.tsx
+++ usg-website/src/pages/components/Navbar.tsx
@@ -1,36 +1,55 @@
 import React from 'react'
-import logo from "../../logos/USG_Logo_Transparent_PNG.png";
+import logo from "../../logos/cropped_logo.png";
 import { Link } from 'react-router-dom';
+import Button from './Button';
 
 
 function Navbar() {
     return (
-        <header className='flex flex-row w-screen'>
-            <nav className='flex flex-row'>
-                <div className='w-28'>
+        <header className='flex flex-row justify-between w-full p-5 items-center bg-black'>
+            <nav className='flex flex-row justify-start items-center'>
+                <div className='h-20 mr-10'>
                     <Link to="/">
-                        <img src={logo} alt="Logo des Teams" />
+                        <img
+                            src={logo}
+                            alt="Logo des Teams"
+                            className="hover:scale-110 transition-all duration-200 h-full"
+                        />
                     </Link>
                 </div>
-                <ul className='flex flex-row'>
+                <ul className='flex flex-row align-middle space-x-5 text-white text-xl
+                                divide-x-2 divide-gray-600 child:pl-5 child:child:py-1
+                                child:child:px-2 child:child:rounded child:child:text-center
+                                child:child-hover:bg-white child:child-hover:text-black
+                                child:child:transition-all child:child:duration-200'>
                     <li>
-                        <Link to="/">Home</Link>
+                        <div>
+                            <Link to="/">Home</Link>
+                        </div>
                     </li>
                     <li>
-                        <Link to="feed">Feed</Link>
+                        <div>
+                            <Link to="feed">Feed</Link>
+                        </div>
                     </li>
                     <li>
-                        <Link to="kontakt">Kontakt</Link>
+                        <div>
+                            <Link to="kontakt">Kontakt</Link>
+                        </div>
                     </li>
                     <li>
-                        <Link to="ueber-uns">Über Uns</Link>
+                        <div>
+                            <Link to="ueber-uns">Über Uns</Link>
+                        </div>
                     </li>
                     <li>
-                        <Link to="unser-team">Unser Team</Link>
+                        <div>
+                            <Link to="unser-team">Unser Team</Link>
+                        </div>
                     </li>
                 </ul>
             </nav>
-
+            <Button text='Mitglied werden' destination='kontakt/bewerben' />
         </header>
     )
 }
