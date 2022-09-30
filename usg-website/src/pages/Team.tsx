59bdcf40df5cf88936dce1f70d49ec38db6c81d5
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-30 21:29:13 +0200

    Revert "add Flipcards as Component and add 4-flipcards to unser team page"
    This reverts commit c51e90511ea2f1fb139575686eadeaac7547af90.

--- usg-website/src/pages/Team.tsx
+++ usg-website/src/pages/Team.tsx
@@ -1,43 +1,15 @@
-import React from "react";
+import React from 'react';
 import Logo from "./components/Logo";
-import Slogan from "./components/Slogan";
-import Membercard from "./components/Membercard";
-import Member from "../logos/USG_Logo_Transparent_PNG.png";
+import Slogan from './components/Slogan';
 
 function Team() {
   return (
-    <div className="app-header">
+    <div className='app-header'>
       <Logo />
       <Slogan />
-      <h1 className="text-pink-600">Unser Team</h1>
-      <div className="flex flex-row">
-        <Membercard
-          mbr={Member}
-          name="Nikknez"
-          function="Mitglied"
-          comment="Er ist toll"
-        />
-        <Membercard
-          mbr={Member}
-          name="Pseudonym"
-          function="Mitglied"
-          comment="Comment"
-        />
-        <Membercard
-          mbr={Member}
-          name="Pseudonym"
-          function="Mitglied"
-          comment="Comment"
-        />
-        <Membercard
-          mbr={Member}
-          name="Pseudonym"
-          function="Mitglied"
-          comment="Comment"
-        />
-      </div>
+      <h1 className='text-pink-600'>Unser Team</h1>
     </div>
-  );
+  )
 }
 
-export default Team;
+export default Team
