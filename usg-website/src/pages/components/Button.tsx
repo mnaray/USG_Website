3de58e60ccd5c79320bdc8a3d955e098929fe839
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-09-30 10:49:10 +0200

    animate button

--- usg-website/src/pages/components/Button.tsx
+++ usg-website/src/pages/components/Button.tsx
@@ -2,14 +2,19 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 interface ButtonProps {
-    text: string;
-    destination: string;
+  text: string;
+  destination: string;
 }
 
 function Button(props: ButtonProps) {
   return (
     <Link to={props.destination}>
-      <button className="bg-blue-300 px-3 py-1 rounded">{props.text}</button>
+      <button className="bg-blue-600 text-white text-lg px-3 py-1 rounded
+                          hover:bg-blue-200 hover:text-black transition-all
+                          duration-200"
+        tabIndex={-1}>
+        {props.text}
+      </button>
     </Link>
   )
 }
