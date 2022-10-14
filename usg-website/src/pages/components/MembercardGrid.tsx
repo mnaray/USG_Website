ffb87c66ddd535e9a34234a49a4151a1c9b69af5
n¿co <89131248+h0peRL@users.noreply.github.com>
2022-10-14 12:27:16 +0200

    formatted MembercardGrid

--- usg-website/src/pages/components/MembercardGrid.tsx
+++ usg-website/src/pages/components/MembercardGrid.tsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import Membercard from './Membercard'
+import React from "react";
+import Membercard from "./Membercard";
 import MemberFoto from "../../logos/USG_Logo_Transparent_PNG.png";
 
 function MembercardGrid() {
-    return (
-        <div className="flex flex-row">
-            <Membercard
-                mbr={MemberFoto}
-                name="Nikknez"
-                function="Mitglied"
-                comment="Er ist toll"
-            />
-            <Membercard
-                mbr={MemberFoto}
-                name="Pseudonym"
-                function="Mitglied"
-                comment="Comment"
-            />
-            <Membercard
-                mbr={MemberFoto}
-                name="Pseudonym"
-                function="Mitglied"
-                comment="Comment"
-            />
-            <Membercard
-                mbr={MemberFoto}
-                name="Pseudonym"
-                function="Mitglied"
-                comment="Comment"
-            />
-        </div>
-    )
+  return (
+    <div className="flex flex-row">
+      <Membercard
+        mbr={MemberFoto}
+        name="Nikknez"
+        function="Mitglied"
+        comment="Er ist toll"
+      />
+      <Membercard
+        mbr={MemberFoto}
+        name="Pseudonym"
+        function="Mitglied"
+        comment="Comment"
+      />
+      <Membercard
+        mbr={MemberFoto}
+        name="Pseudonym"
+        function="Mitglied"
+        comment="Comment"
+      />
+      <Membercard
+        mbr={MemberFoto}
+        name="Pseudonym"
+        function="Mitglied"
+        comment="Comment"
+      />
+    </div>
+  );
 }
 
-export default MembercardGrid
+export default MembercardGrid;
