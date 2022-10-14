4df81767aa05085ff795ad57b1220658dc8a9471
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-10-14 13:10:49 +0200

    create welcome component

---
+++ usg-website/src/pages/components/Willkommenstext.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Willkommenstext() {
+    return (
+        <div className='text-3xl text-center w-1/3'>
+            <p className='mb-2'>
+                Willkommen auf unserer Homepage!
+            </p>
+            <p className='text-xl'>
+                Bewirb dich bie uns oder vordere uns zu einem Scrim heraus.
+                Du kannst aber auch ein wenig mehr über uns und unser Team
+                herausfinden.
+            </p>
+        </div>
+    )
+}
+
+export default Willkommenstext
