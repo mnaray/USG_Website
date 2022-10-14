a56b174c9e5d6617ecc6d58aead8f2fbf6f0bb71
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-10-14 13:28:16 +0200

    clean up homepage

--- usg-website/src/pages/Home.tsx
+++ usg-website/src/pages/Home.tsx
@@ -3,6 +3,7 @@ import "../css/tailwind.css";
 import Logo from "./components/Logo";
 import Slogan from "./components/Slogan";
 import Willkommenstext from "./components/Willkommenstext";
+import Button from "./components/Button";
 
 function Home() {
   return (
@@ -10,6 +11,13 @@ function Home() {
       <Logo />
       <Slogan />
       <Willkommenstext />
+      <div className="flex flex-row justify-evenly w-1/4 py-8">
+        <Button text="Bewirb Dich!" destination="kontakt/bewerben" />
+        <p className="text-2xl">,</p>
+        <Button text="Scrim Us!" destination="kontakt/scrim-us" />
+        <p className="text-2xl">oder</p>
+        <Button text="Über Uns" destination="ueber-uns" />
+      </div>
     </main>
   );
 }
