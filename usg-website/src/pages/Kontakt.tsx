0eddea5009196922db19a7db33616974776159b3
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-10-27 21:40:31 +0200

    finish contact page

--- usg-website/src/pages/Kontakt.tsx
+++ usg-website/src/pages/Kontakt.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
-import Logo from "./components/Logo";
-import Slogan from './components/Slogan';
+import GoogleForms from './components/GoogleForms';
+import Button from './components/Button';
 
 function Kontakt() {
   return (
     <main>
-      <Logo />
-      <Slogan />
-      <h1 className='text-pink-600'>Kontakt</h1>
+      <h1 className="text-3xl">Kontakt</h1>
+      <p className='text-xl text-justify w-1/3 py-8'>
+        Du kannst uns auf verschiedene Arten und Weisen kontaktieren. Um
+        dich bei uns zu bewerben, kannst du nach unten scrollen und unser
+        Formular ausfüllen. Wenn du gegen uns scrimmen willst, klickst du
+        auf den untenstehenden Button "Scrim Us!". Alternativ kannst du
+        uns auch auf Social-Media erreichen.
+      </p>
+      <div className="flex flex-row justify-evenly min-w-fit py-12">
+        <Button text='Bewirb Dich!' destination='bewerben' />
+        <p className='text-2xl px-3'>oder</p>
+        <Button text='Scrim Us!' destination='scrim-us' />
+      </div>
+      <h1 className='text-2xl pt-5'>Kontakformular allgemein</h1>
+      <GoogleForms url='https://docs.google.com/forms/d/e/1FAIpQLSes1z9E7D4g9afB-2oP27qLo1iv_mKeahRVAIIiJFQzR-JKmw/viewform?embedded=true' />
     </main>
-  )
+  );
 }
 
 export default Kontakt
