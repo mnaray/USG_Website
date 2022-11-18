6d087f56fa6d9ed0889f0d437566bf6faaca5f32
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-18 08:41:15 +0100

    comment out unused import

--- usg-website/src/index.tsx
+++ usg-website/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import Router from "./pages/Router";
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
