b6f55eb023069ef99d398f2761f998245fcb1796
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-18 07:55:50 +0100

    lorem ipsum text entfernt

--- usg-website/src/pages/Team.tsx
+++ usg-website/src/pages/Team.tsx
@@ -6,14 +6,6 @@ function Team() {
   return (
     <main>
       <Title title="Unser Team" />
-      <div className="flex flex-col items-center w-1/3">
-        <p className="justify-center text-lg">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
-          libero magni et, fugit, aperiam ipsa fugiat dignissimos reprehenderit
-          at laboriosam a ex corrupti error placeat aut neque blanditiis velit
-          debitis.
-        </p>
-      </div>
       <MembercardGrid />
     </main>
   )
