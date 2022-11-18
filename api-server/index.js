db3f31a80179f98c3274dc667885be9fc51eaee3
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-18 21:28:20 +0100

    remove origin restriction for development

--- api-server/index.js
+++ api-server/index.js
@@ -11,7 +11,7 @@ const imageFiles = deta.Drive("images");
 // express
 const app = express();
 app.use(express.json()); // !!| ESSENTIAL FOR req |!!
-app.use(cors({ origin: "https://usginfo.ch"})) // Cors
+app.use(cors()) // Cors
 app.disable("etag"); // disables automatic caching
 
 app.get("/", (req, res) => res.send("This is the API for usginfo.ch"));
