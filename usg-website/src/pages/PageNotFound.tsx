9564d199455f55ff0a575d5bfa2c6da07a7af29f
n¿co <89131248+h0peRL@users.noreply.github.com>
2022-11-18 08:29:26 +0100

    changed css of Page not Found

--- usg-website/src/pages/PageNotFound.tsx
+++ usg-website/src/pages/PageNotFound.tsx
@@ -4,8 +4,8 @@ import Logo from './components/Logo';
 function PageNotFound() {
     return (
         <main>
-            <h1 className='text-red-600'>Error 404: Page not found!</h1>
-            <p>Bitte eine valide URL eingeben.</p>
+            <h1 className='text-red-600 text-4xl'>Error 404: Page not found!</h1>
+            <p className='p-6 text-xl'>Bitte eine valide URL eingeben.</p>
             <Logo />
         </main>
     );
