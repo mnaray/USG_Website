80cd3575d9c34f4fcf7620968e544be40bb50dd8
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-22 15:54:26 +0100

    create info route for members

--- api-server/routes/members.js
+++ api-server/routes/members.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", (req, res) => {
-    res.status(200).send("This is the members route");
+router.get("/info", (req, res) => {
+    res.status(200).send(
+        "This route is reserved for everything related to team members."
+    );
 });
 
 module.exports = router;
