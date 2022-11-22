b7548966f9c1f4a995349d1d7379e922eb96e54d
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-22 15:50:44 +0100

    add new members route

--- api-server/index.js
+++ api-server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const upload = require("express-fileupload");
 const { Deta } = require("deta");
-const cors = require("cors")
+const cors = require("cors");
 
 // deta
 const deta = Deta();
@@ -11,11 +11,14 @@ const imageFiles = deta.Drive("images");
 // express
 const app = express();
 app.use(express.json()); // !!| ESSENTIAL FOR req |!!
-app.use(cors()) // Cors
+app.use(cors()); // Cors
 app.disable("etag"); // disables automatic caching
 
+// routes
+app.use("/members", require("./routes/members"));
+
+// root
 app.get("/", (req, res) => res.send("This is the API for usginfo.ch"));
 
 // export 'app'
 module.exports = app;
-
