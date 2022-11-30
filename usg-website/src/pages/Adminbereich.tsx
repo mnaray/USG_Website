5d89a0cdc630cbd50d629e859fcfc976d8194b24
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-11-30 18:11:59 +0100

    create button for adding new members

--- usg-website/src/pages/Adminbereich.tsx
+++ usg-website/src/pages/Adminbereich.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Adminlist from './components/Adminlist'
+import CircularButton from './components/CircularButton'
 
 function Adminbereich(): JSX.Element {
     return (
         <main>
             <Adminlist />
+            <CircularButton destination='hinzufuegen' />
         </main>
     )
 }
