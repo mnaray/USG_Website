d95c960c20876c16e502df1decd38a515ab8123c
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-02 10:13:18 +0100

    add conflict avoidance system to editing page

--- usg-website/src/pages/Bearbeiten.tsx
+++ usg-website/src/pages/Bearbeiten.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import InputForm from './components/InputForm'
 import Title from './components/Title'
 
@@ -12,14 +12,38 @@ interface member {
 }
 
 interface memberCurrent {
-    member: member;
+    memberKey: string;
 }
 
 function Bearbeiten(props: memberCurrent) {
+
+    const [member, setMember] = useState<member>({
+        key: "-",
+        name: "Loading...",
+        funktionIG: "-",
+        teamrolle: "-",
+        comment: "-",
+        imgPath: "",
+    })
+
+    const getMemberData = async () => {
+        const response = await fetch("https://api.usginfo.ch/members/" + props.memberKey, {
+            method: "GET"
+        });
+
+        try {
+            const responseJson: member = await response.json();
+            setMember(responseJson);
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
     return (
         <main>
             <Title>Mitglied Bearbeiten</Title>
-            <InputForm method='put' memberCurrent={props.member} />
+            <InputForm method='put' memberCurrent={member} />
         </main>
     )
 }
