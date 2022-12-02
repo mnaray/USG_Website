83640c678b5f0432f822d1517587b17c4a3e71b8
n¿co <89131248+h0peRL@users.noreply.github.com>
2022-12-02 08:56:27 +0100

    rounded

--- usg-website/src/pages/components/Login.tsx
+++ usg-website/src/pages/components/Login.tsx
@@ -6,9 +6,9 @@ function Login() {
         <div className="">
             <form className='flex flex-col items-center'>
                 <label htmlFor="name">Username: </label>
-                <input type="text" name="username" className='text-neutral-800 m-2 p-2 bg-slate-500' id="username" placeholder='username' required minLength={4} />
+                <input type="text" name="username" className='text-neutral-800 m-2 p-2 bg-slate-500 rounded' id="username" placeholder='username' required minLength={4} />
                 <label htmlFor="password">Password: </label>
-                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500' id="password" name='password' placeholder='password' required minLength={8} />
+                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500 rounded' id="password" name='password' placeholder='password' required minLength={8} />
                 <button type="submit" className='bg-gray-900 p-2 rounded'>Submit</button>
             </form>
             <p className="m-5">Kein Konto? <Button text="Registrieren" destination="../register" /></p>
--- usg-website/src/pages/components/Register.tsx
+++ usg-website/src/pages/components/Register.tsx
@@ -6,11 +6,11 @@ function Register() {
         <div className="">
             <form className='flex flex-col items-center'>
                 <label htmlFor="name">Username: </label>
-                <input type="text" name="username" className='text-neutral-800 m-2 p-2 bg-slate-500' id="username" placeholder='username' required minLength={4} />
+                <input type="text" name="username" className='text-neutral-800 m-2 p-2 bg-slate-500 rounded' id="username" placeholder='username' required minLength={4} />
                 <label htmlFor="password">Password: </label>
-                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500' id="password" name='password' placeholder='password' required minLength={8} />
+                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500 rounded' id="password" name='password' placeholder='password' required minLength={8} />
                 <label htmlFor="password">Password wiederholen: </label>
-                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500' id="passwordagain" name='passwordagain' placeholder='password wiederholen' required minLength={8} />
+                <input type="password" className='text-neutral-800 m-2 p-2 bg-slate-500 rounded' id="passwordagain" name='passwordagain' placeholder='password wiederholen' required minLength={8} />
                 <button type="submit" className='bg-gray-900 p-2 rounded'>Submit</button>
             </form>
             <p className="m-5">Breits ein Konto? <Button text="Anmelden" destination="../login" /></p>
