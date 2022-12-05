ee01322808bcfacc61819d7584427178ade8c552
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-05 18:35:17 +0100

    add redirect 1.5 seconds after submitting new user

--- usg-website/src/pages/components/InputForm.tsx
+++ usg-website/src/pages/components/InputForm.tsx
@@ -62,12 +62,12 @@ function InputForm(props: inputFormat) {
                 <AdminInputField label='Teamrolle' placeholder='Teamrollen eingeben' optional={false} default={props.memberCurrent?.teamrolle} />
                 <AdminInputField label='Funktion-IG' placeholder='In-Game Funktion eingeben' optional={true} default={props.memberCurrent?.funktionIG} />
                 <AdminInputField label='Kommentar' placeholder='Kommentar eingeben' optional={true} default={props.memberCurrent?.comment} />
-                <SubmitButton>Speichern</SubmitButton>
+                <SubmitButton redirect={true} >Speichern</SubmitButton>
                 <p className={status[0]}>{status[1]}</p>
             </form>
             <form method={props.method} encType='multipart/form-data' className='flex flex-col justify-between mt-10'>
                 <input type="file" name="file" className='mb-5' />
-                <SubmitButton>Update</SubmitButton>
+                <SubmitButton redirect={false} >Update</SubmitButton>
             </form>
         </div>
     )
--- usg-website/src/pages/components/SubmitButton.tsx
+++ usg-website/src/pages/components/SubmitButton.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 interface submitButton {
     children: string;
+    redirect: boolean;
 }
 
 function SubmitButton(props: submitButton) {
+
+    const navigate = useNavigate();
+    const redirectUser = (event: MouseEvent<HTMLInputElement>) => {
+        if (props.redirect) setTimeout(() => navigate("/admin"), 1500);
+    }
+
     return (
         <input
             type="submit"
             value={props.children}
+            onClick={redirectUser}
             className='bg-blue-600 rounded-md w-1/2 p-1 text-lg hover:bg-blue-200
-                    hover:text-black transition-all duration-200' />
+                    hover:text-black transition-all duration-200'
+        />
     )
 }
 
