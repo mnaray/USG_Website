2952fc3969c417d35277d0c8b57b3ba01aaaffd8
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-16 10:53:43 +0100

    remove faulty flex class from burgermenu

--- usg-website/src/pages/components/BurgerMenu.tsx
+++ usg-website/src/pages/components/BurgerMenu.tsx
@@ -6,7 +6,7 @@ import NavLinksBurger from "./NavLinksBurger";
 function BurgerMenu() {
   const [open, setOpen] = useState(false);
   return (
-    <header className="flex flex-col justify-center w-full p-5 hidden md:flex items-center bg-black">
+    <header className="flex-col justify-center w-full p-5 hidden md:flex items-center bg-black">
       <nav className="flex flex-col justify-start items-center">
         <CgMenuRound
           className="text-5xl text-start cursor-pointer"
