a884cc05a5882441df6e64004e0ad62b144c88b6
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-17 11:34:59 +0100

    fix issue with image of first member not rendering

--- usg-website/src/pages/components/Membercard.tsx
+++ usg-website/src/pages/components/Membercard.tsx
@@ -52,7 +52,7 @@ function Membercard(source: membercard) {
 
     try {
       const responseJson: filesResponse = await response.json();
-      const arrayBuffer: ArrayBuffer = await bufferToArrayBuffer(responseJson.data);
+      const arrayBuffer: ArrayBuffer = bufferToArrayBuffer(responseJson.data);
       const base64: string = await arrayBufferToBase64(arrayBuffer);
       setMemberImage(base64);
     } catch (err) {
@@ -62,7 +62,7 @@ function Membercard(source: membercard) {
 
   useEffect(() => {
     getMemberImage(source.img);
-  }, [])
+  })
 
   return (
     <div className="flip-card m-10 rounded">
