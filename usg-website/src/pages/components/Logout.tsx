603779d367a02d069512914a59513c6a5105efc0
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-19 13:25:35 +0100

    add logout button to adminpage

--- usg-website/src/pages/components/Logout.tsx
+++ usg-website/src/pages/components/Logout.tsx
@@ -1,25 +1,11 @@
-import React, { MouseEvent } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import LogoutButton from './LogoutButton';
 
 function Logout() {
-    const navigate = useNavigate();
-
-    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-        sessionStorage.removeItem("token");
-        navigate("/")
-    }
-
     return (
         <div className='flex flex-col items-center'>
             <p className='text-xl mb-4'>Du bist schon eingeloggt! Keine Lust mehr?</p>
-            <button onClick={handleClick}
-                className="bg-red-600 text-white text-lg px-3 py-1 mt-3 rounded
-                            hover:bg-blue-200 hover:text-black transition-all
-                            duration-200 w-fit"
-            >
-                Logout
-            </button>
+            <LogoutButton />
         </div>
     )
 }
